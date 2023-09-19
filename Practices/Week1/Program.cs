f3d15c3fca50a66e3631e8f8c5d21b6e41bce5ba
Esmanur Mazlum <esmanurmazlumm@gmail.com>
2023-09-19 19:23:19 +0300

    instance and property notions

---
+++ Practices/Week1/Program.cs
@@ -0,0 +1,24 @@
+﻿
+
+using Week1;
+
+Student student1 = new Student()
+{
+    Id = 26029,
+    Name = "Fatma Özlem",
+    Surname = "Acar"
+};
+
+Student student2 = new Student()
+{
+    Id = 66435,
+    Name = "Aygün",
+    Surname = "Bayram"
+};
+
+//student1.Id = 26029;
+//student1.Name = "Fatma Özlem";
+//student1.Surname = "Acar";
+
+Console.WriteLine($"İsim : {student1.Name} Soyisim : {student1.Surname}");
+Console.WriteLine($"İsim : {student2.Name} Soyisim : {student2.Surname}");
---
+++ Practices/Week1/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week1
+{
+    internal class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+    }
+}
