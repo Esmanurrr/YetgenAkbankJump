6abdad3de0c0e653a3a9722bf11472cc7c371183
Esmanur Mazlum <esmanurmazlumm@gmail.com>
2023-09-23 22:23:51 +0300

    introduction to encapsulation

---
+++ Practices/Week2/Entities/Account.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week2.Entities
+{
+    internal class Account
+    {
+       
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public decimal Balance { get; set; }
+
+        public Account(string name, string surname)
+        {
+            Id= Guid.NewGuid();
+            Name = name;
+            Surname = surname;
+            Balance = 100000;
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            if(Balance - amount >= 0)
+            {
+                Balance -= amount;
+                Console.WriteLine($"Withdraw Amount: {amount}");
+            }
+            else
+            {
+                Console.WriteLine("Not enough money");
+            }
+        }
+    }
+}
---
+++ Practices/Week2/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Week2.Entities;
+
+Console.WriteLine("Encapsulation");
+
+Account account1 = new("Oğuz", "Sağlam");
+account1.Withdraw(2000);
+account1.Withdraw(200);
+
+Console.WriteLine($"Balance: {account1.Balance}");
