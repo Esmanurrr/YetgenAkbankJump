2a550338f5749321e3e710b86ec649e8ec5b8bbb
Esmanur Mazlum <esmanurmazlumm@gmail.com>
2023-09-25 10:16:29 +0300

    refactored Playlist class

---
+++ Practices/Week2_2/Entities/Playlist.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week2_2.Entities
+{
+    internal class Playlist
+    {
+        public Guid Id { get; set; }
+        private List<string> Songs { get; set; }
+
+        private Playlist()
+        {
+            Songs = new List<string>();
+        }
+
+        public Playlist(string firstSong):this() 
+        {
+            AddSong(firstSong);
+        }
+
+        public void AddSong(string song)
+        {
+            if(IsSongTitleValid(song))
+                Songs.Add(song);
+        }
+
+        private bool IsSongTitleValid(string song)
+        {
+            return !string.IsNullOrEmpty(song)
+                && song.Length > 2
+                && song.Length < 20;
+        }
+    }
+}
---
+++ Practices/Week2_2/Program.cs
@@ -0,0 +1,9 @@
+﻿
+
+using Week2_2.Entities;
+
+Playlist playlist = new();
+
+playlist.AddSong("Neye Yarar");
+playlist.AddSong("");
+playlist.AddSong("   ");
