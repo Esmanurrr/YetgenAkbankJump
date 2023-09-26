0f1167e63a9523fe8e499577f8ac825c849fd507
Esmanur Mazlum <esmanurmazlumm@gmail.com>
2023-09-26 08:58:12 +0300

    freelancer app - week 3

---
+++ Freelancer/Entities/Customer.cs
@@ -0,0 +1,36 @@
+﻿using Freelancer.Abstract;
+using Freelancer.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Freelancer.Entities
+{
+    internal class Customer : Person<Guid>, ICsvConvertible
+    {
+        public string PhoneNumber { get; set; }
+
+
+        public string GetValuesCSV()
+        {
+            return $"{Id},{CreatedOn},{FirstName},{LastName},{PhoneNumber}";
+        }
+
+        public void SetValueCSV(string csvLine)
+        {
+            string[] data = csvLine.Split(',');
+
+            Id = Guid.Parse(data[0]);
+            CreatedOn = DateTimeOffset.Parse(data[1]);
+            FirstName = data[2];
+            LastName = data[3];
+            PhoneNumber = data[4];
+
+        }
+
+    }
+
+    
+}
---
+++ Freelancer/Services/NotepadService.cs
@@ -0,0 +1,34 @@
+﻿using Freelancer.Abstract;
+using Freelancer.Constants;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Freelancer.Services
+{
+    internal class NotepadService
+    {
+        public void SaveToNotepad(ICsvConvertible data)
+        {
+            string path = $"{FileLocations.ProjectFolder}\\Database";
+            string type = data.GetType().ToString().Split(".").LastOrDefault();
+
+            if(!Directory.Exists(path))
+                Directory.CreateDirectory(path);
+
+            string filePath = $"{path}\\{type}s.txt";
+
+            File.AppendAllText(filePath, $"{data.GetValuesCSV()}\n");
+        }
+
+        public string GetOnNotepad(string path)
+        {
+            if(File.Exists(path))
+                return File.ReadAllText(path);
+
+            throw new Exception("File doesn't exist");
+        }
+    }
+}
