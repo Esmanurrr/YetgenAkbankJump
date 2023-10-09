71f458ce542411a59d7119849f3a9e1ed10e8cec
Esmanur Mazlum <esmanurmazlumm@gmail.com>
2023-10-09 11:37:30 +0300

    added Entity Framework Core practice

---
+++ Practices/EntityFrameworkPractice/Persistence/NoteMasterDbContext.cs
@@ -0,0 +1,20 @@
+﻿using EntityFrameworkPractice.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkPractice.Persistence
+{
+    public class NoteMasterDbContext : DbContext
+    {
+        public DbSet<Note> Notes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseInMemoryDatabase("NoteMaster");
+        }
+    }
+}
---
+++ Practices/EntityFrameworkPractice/Program.cs
@@ -0,0 +1,16 @@
+﻿
+using EntityFrameworkPractice.Entities;
+using EntityFrameworkPractice.Persistence;
+
+Console.WriteLine("Entity Framework Core - InMemory");
+
+NoteMasterDbContext _context = new();
+_context.Notes.Add(new("Note 1"));
+_context.Notes.Add(new("Note 2"));
+_context.Notes.Add(new("Note 3"));
+
+_context.SaveChanges();
+
+List<Note> notes = _context.Notes.ToList();
+
+Console.WriteLine();
