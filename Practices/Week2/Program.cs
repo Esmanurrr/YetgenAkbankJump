875d2659f61348358a1934b5f8141fbd3d2f91eb
Esmanur Mazlum <esmanurmazlumm@gmail.com>
2023-10-16 11:42:26 +0300

    modified

--- Practices/Week2/Program.cs
+++ Practices/Week2/Program.cs
@@ -4,7 +4,6 @@ using Week2.Entities;
 Console.WriteLine("Encapsulation");
 
 Account account1 = new("Oğuz", "Sağlam");
-account1.Withdraw(2000);
-account1.Withdraw(200);
+
 
 Console.WriteLine($"Balance: {account1.Balance}");
