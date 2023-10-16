92ee267d19d5011824378651e4d6b8177fc69356
Esmanur Mazlum <esmanurmazlumm@gmail.com>
2023-10-16 11:43:02 +0300

    Code first initialize

---
+++ Practices/Week_5_1/Contexts/ShopifyDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Week_5_1.Entities;
+
+namespace Week_5_1.Contexts
+{
+    public class ShopifyDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Server=91.151.83.102;Port=5432;Database=ShopifyEsmanurMazlum;User Id=ahmetkokteam;Password=obXRMG*U6rJ4R0cbHszpgEuFd;");
+        }
+
+
+    }
+
+}
