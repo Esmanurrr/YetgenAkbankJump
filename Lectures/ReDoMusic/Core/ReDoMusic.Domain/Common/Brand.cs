a446744e3925153e8ac998e34b9b929d33cf5455
Esmanur Mazlum <esmanurmazlumm@gmail.com>
2023-10-18 20:42:17 +0300

    added entities

---
+++ Lectures/ReDoMusic/Core/ReDoMusic.Domain/Common/Brand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReDoMusic.Domain.Common
+{
+    public class Brand : EntityBase<Guid>
+    {
+        public string Name { get; set; }
+        public string DisplayText { get; set; }
+        public string Address { get; set; }
+        //public DateTime EstablishDate  { get; set; }
+    }
+}
--- Lectures/ReDoMusic/Presentation/ReDoMusic.MVC/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ReDoMusic.MVC.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
