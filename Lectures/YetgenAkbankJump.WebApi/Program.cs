fc5b61da6d6a490a23d104bc9a74c92e0698d6c8
Esmanur Mazlum <esmanurmazlumm@gmail.com>
2023-10-19 21:47:19 +0300

    week 6 section 2 - blazor page

--- Lectures/YetgenAkbankJump.WebApi/Program.cs
+++ Lectures/YetgenAkbankJump.WebApi/Program.cs
@@ -7,7 +7,20 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        builder => builder
+            .AllowAnyMethod()
+            .AllowCredentials()
+            .SetIsOriginAllowed((host) => true)
+            .AllowAnyHeader());
+});
+
 var app = builder.Build();
+
+app.UseCors("AllowAll");
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -15,6 +28,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
 
 app.UseHttpsRedirection();
 
