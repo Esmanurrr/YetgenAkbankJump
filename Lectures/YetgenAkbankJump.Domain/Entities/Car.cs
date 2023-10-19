42b9848b22bf691ea1cee8cbe96085f8763b7913
Esmanur Mazlum <esmanurmazlumm@gmail.com>
2023-10-19 21:04:21 +0300

    week 6 section 1

---
+++ Lectures/YetgenAkbankJump.Domain/Entities/Car.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YetgenAkbankJump.Domain.Entities
+{
+    public class Car
+    {
+        public Guid Id { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public decimal Price { get; set; }
+        public string ImageUrl { get; set; }
+
+    }
+}
---
+++ Lectures/YetgenAkbankJump.WebApi/Controllers/CarsController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using YetgenAkbankJump.WebApi.Data;
+
+namespace YetgenAkbankJump.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarsController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var cars = CarsContext.luxuryCars.ToList();
+
+            return Ok(cars);
+        }
+
+        [HttpGet("{id:guid}")]
+        public IActionResult GetById(Guid id)
+        {
+
+            if (id == Guid.Empty)
+                return BadRequest("Id cannot be empty.");
+
+            var car = CarsContext.luxuryCars.FirstOrDefault(x=>x.Id == id);
+
+            if (car is null)
+                return NotFound("The car requested with given Id was not found.");
+
+            return Ok(car);
+        }
+    }
+}
