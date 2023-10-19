126bbf3da5391f46c344069e027f2a7f37629239
Esmanur Mazlum <esmanurmazlumm@gmail.com>
2023-10-19 19:37:37 +0300

    added controller

---
+++ Lectures/ReDoMusic/Presentation/ReDoMusic.MVC/Controllers/BrandController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ReDoMusic.Domain.Entities;
+using ReDoMusic.Persistence.Contexts;
+
+namespace ReDoMusic.MVC.Controllers
+{
+    public class BrandController : Controller
+    {
+        private readonly ReDoMusicDbContext _context;
+
+        public BrandController()
+        {
+            _context = new();
+        }
+
+        public IActionResult Index()
+        {
+            var brands = _context.Brands.ToList();
+            return View(brands);
+        }
+
+        [HttpGet]
+        public IActionResult AddBrand()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddBrand(string brandName, string brandDisplayText, string address)
+        {
+            var brand = new Brand()
+            {
+                Id = Guid.NewGuid(),
+                Name = brandName,
+                Address = address,
+                DisplayText = brandDisplayText,
+                CreatedOn = DateTime.UtcNow
+            };
+
+            _context.Brands.Add(brand);
+            _context.SaveChanges();
+
+
+            return View();
+        }
+
+    }
+}
