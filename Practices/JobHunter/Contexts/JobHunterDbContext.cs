da9d03e11b9e5d8e3ccdc2d8ffe9d4861909a9d4
Esmanur Mazlum <esmanurmazlumm@gmail.com>
2023-10-22 21:23:45 +0300

    added context

---
+++ Practices/JobHunter/Contexts/JobHunterDbContext.cs
@@ -0,0 +1,20 @@
+﻿using JobHunter.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobHunter.Contexts
+{
+    public class JobHunterDbContext : DbContext
+    {
+        public DbSet<JobPost> JobPosts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql();
+        }
+    }
+}
--- Practices/JobHunter/Program.cs
+++ Practices/JobHunter/Program.cs
@@ -1,4 +1,5 @@
 ﻿
+using JobHunter.Contexts;
 using JobHunter.Entities;
 using JobHunter.Enums;
 
@@ -7,12 +8,16 @@ Console.WriteLine("Change Tracker");
 JobPost jobPost1 = new()
 {
     Title = "Junior Developer",
-  
+
     Company = "FoolTech",
-    
+
     Description = "Bachelor's Degree in Computer Science/Engineering or relevant areas, Minimum 3 years experience in Net Framework & C# programming language (Knowledge and experience in .Net Core is a big plus)",
 
     WorkMode = WorkMode.InOffice,
-   
+
     CreatedOn = DateTime.Now,
-}
+};
+
+JobHunterDbContext _context = new();
+_context.JobPosts.Add(jobPost1);
+_context.SaveChanges();
