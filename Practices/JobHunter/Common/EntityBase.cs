8345223a0820cd623bbbd8fefde7fa2d4cacd11a
Esmanur Mazlum <esmanurmazlumm@gmail.com>
2023-10-22 20:29:20 +0300

    added JobHunter project files

---
+++ Practices/JobHunter/Common/EntityBase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobHunter.Common
+{
+    public class EntityBase<TKey>
+    {
+        public TKey Id { get; set; }
+        public DateTime CreatedOn { get; set; }
+    }
+}
