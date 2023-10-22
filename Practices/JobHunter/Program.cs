08d63c77bdfb7882fe0492f0edf33108754ecb46
Esmanur Mazlum <esmanurmazlumm@gmail.com>
2023-10-22 20:33:24 +0300

    added instance

--- Practices/JobHunter/Program.cs
+++ Practices/JobHunter/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using JobHunter.Entities;
+using JobHunter.Enums;
+
+Console.WriteLine("Change Tracker");
+
+JobPost jobPost1 = new()
+{
+    Title = "Junior Developer",
+  
+    Company = "FoolTech",
+    
+    Description = "Bachelor's Degree in Computer Science/Engineering or relevant areas, Minimum 3 years experience in Net Framework & C# programming language (Knowledge and experience in .Net Core is a big plus)",
+
+    WorkMode = WorkMode.InOffice,
+   
+    CreatedOn = DateTime.Now,
+}
