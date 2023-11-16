c5250b4136ec424a6beba5c96866fa7666a84bbc
Esmanur Mazlum <esmanurmazlumm@gmail.com>
2023-11-16 19:50:39 +0300

    Added Singleton Example

---
+++ Practices/SingletonExample/ConfigurationService.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonExample
+{
+    public class ConfigurationService
+    {
+        private static ConfigurationService instance;
+
+        public static ConfigurationService GetInstance()
+        {
+            if(instance is null)
+                instance = new ConfigurationService();
+
+            return instance;
+        }
+
+
+        public string GetValue(string key)
+        {
+            ConfigurationManager configurationManager = new();
+
+            string path = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName;
+
+            configurationManager.SetBasePath(path);
+
+            configurationManager.AddJsonFile("Configuration.json");
+
+            return configurationManager.GetSection(key).Value;
+
+        }
+
+        ConfigurationService()
+        {
+            Console.WriteLine("Instance Created!");
+        }
+    }
+}
---
+++ Practices/SingletonExample/Program.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.Configuration;
+using SingletonExample;
+
+Console.WriteLine("Singleton");
+
+ConfigurationService configurationService = ConfigurationService.GetInstance();
+ConfigurationService.GetInstance();
+ConfigurationService.GetInstance();
+ConfigurationService.GetInstance();
+ConfigurationService.GetInstance();
+
+var exampleConnectionString = configurationService.GetValue("ConnectionStrings:ExampleDb");
