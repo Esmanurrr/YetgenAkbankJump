393dc152243639281d509d5afb4b4419275753fe
Esmanur Mazlum <esmanurmazlumm@gmail.com>
2023-12-08 23:10:16 +0300

    Created RepositoryPattern project

---
+++ Lectures/RepositoryPattern/Core/RepositoryPattern.Domain/Entities/Product.cs
@@ -0,0 +1,15 @@
+﻿using RepositoryPattern.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoryPattern.Domain.Entities
+{
+    public class Product : EntityBase
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
---
+++ Lectures/RepositoryPattern/Infrastructure/RepositoryPattern.Persistence/Contexts/RepositoryDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RepositoryPattern.Domain.Entities;
+
+namespace RepositoryPattern.Persistence.Contexts
+{
+    public class RepositoryDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseInMemoryDatabase("RepositoryDb");
+        }
+    }
+}
